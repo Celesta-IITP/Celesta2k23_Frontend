953a2db0c28af607913660146dce91e18466496c
Akhand Pratap Singh <sakhand778@gmail.com>
2023-10-14 19:05:42 +0530

    added events

---
+++ celesta20react/src/components/Event Registration/soloEventRegistration.js
@@ -0,0 +1,51 @@
+import React from 'react'
+// import { useRouter } from 'next/router'
+// const host = process.env.NEXT_PUBLIC_HOST
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
+
+const host="http://127.0.0.1:8000"
+async function soloEventRegistration(eventID, router, closeHandler) {
+    var myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
+
+    var raw = JSON.stringify({
+        event_id: eventID,
+    })
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow',
+        credentials: 'include',
+    }
+
+    const data = await fetch(`${host}/event/registration/solo`, requestOptions)
+        .then((response) => response.json())
+        .catch((error) => {
+            console.error(error)
+        })
+    // .then(result => console.log(result))
+    // .catch(error => console.log('error', error));
+
+    // fetch('http://localhost:1337/razorpay', { method: 'POST' }).then((t) =>
+    // 	t.json()
+    // )
+    if (data.payment_url) {
+        router.push(data.payment_url)
+    } else {
+        closeHandler()
+        toast.success(data.messagge, {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+        })
+    }
+}
+export default soloEventRegistration
---
+++ celesta20react/src/components/EventItem/index.js
@@ -0,0 +1,102 @@
+import Image from "../Image"
+import styles from './eventitem.module.css'
+// import { motion } from 'framer-motion'
+import Modal from '../BigModal/index'
+import React,{ useState } from 'react'
+
+/*  
+event={
+    is_active=bool,
+    poster=string(url of event poster),*
+    name=string,*
+    start_time=Date,*
+    end_time=Date,*
+    venue=string,*EXTRA venueUrl
+    description=string,*
+    min_team_size=number,*
+    max_team_size=munber,*
+    registration_fee=number,*
+    registration_deadline=Date,
+    prize=number,
+    organizer=[[organizer_name,organizer_number]],
+    video=rulebook,*
+    is_online=bool,
+    is_solo=bool,
+    id=any
+}
+*/
+const index = ( {event} ) => {
+    const [modal, setModal] = useState(false)
+    // var model=false
+    const close = () => {
+        setModal(false);
+    }
+
+    return (
+        <>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                    position: 'relative',
+                }}
+                onClick={() => setModal(true)}
+            >
+                {event.is_active ? null : (
+                    <img
+                        src="/assets/coming-soon.png"
+                        style={{
+                            position: 'absolute',
+                            top: '8px',
+                            right: '30px',
+                            zIndex: 2,
+                            height: '100px',
+                        }}
+                        alt="Coming Soon"
+                    />
+                )}
+                <div className={styles.imageWrap}>
+                    <div className={styles.item}>
+                        <img
+                            src={
+                                event.poster
+                                    ? event.poster
+                                    : '/events/poster.png'
+                            }
+                            alt="Fest Image"
+                            width={300}
+                            height={300}
+                            style={{ borderRadius: 15 }}
+                            objectfit={'contain'}
+                        />
+                    </div>
+                    <p className={styles.img__description}>
+                        <span style={{ marginBottom: '10px' }}>
+                            <Image
+                                src="/events/Vector.png"
+                                width={70}
+                                height={70}
+                                alt="Fest Image overlay"
+                            />
+                        </span>{' '}
+                    </p>
+                </div>
+                <div className={styles.eventName}>{event.name}</div>
+            </div>
+            {modal && (
+                <>
+                    <Modal
+                        title={event.name}
+                        body={event}
+                        closeHandler={close}
+                    />
+                </>
+            )}
+        </>
+    )
+}
+
+export default index
