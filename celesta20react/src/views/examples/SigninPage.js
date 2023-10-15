a79f1b5f0ac50156d0c51b59a47b3c1407cf85c1
kanishk <kanishks281@gmail.com>
2023-10-15 21:49:46 +0530

    sign in page

--- celesta20react/src/views/examples/SigninPage.js
+++ celesta20react/src/views/examples/SigninPage.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import  "./RegisterPage.css"
 import classnames from "classnames";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { Alert } from "antd";
+import { Link } from "react-router-dom";
+
 
 // reactstrap components
 import {
@@ -25,302 +26,222 @@ import {
   Container,
   Row,
   Col,
+  Dropdown,
+  ButtonDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  InputGroupButtonDropdown,
 } from "reactstrap";
-import { loginUser } from "redux/actions/authActions";
-import { clearErrors } from "redux/actions/errorActions";
+
 // core components
+import { loginUser } from "redux/actions/authActions";
+// import { clearErrors } from "redux/actions/errorActions";
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footer/Footer.js";
-
+import { USER_LOADING } from "redux/actions/types";
+import { registerUser } from "redux/actions/authActions";
+import { clearErrors } from "redux/actions/errorActions";
+import ValidatedLoginForm from "./ValidateLogin";
+import { serverUrl } from "../../config";
 class SigninPage extends React.Component {
-  state = {
-    squares1to6: "",
-    squares7and8: "",
-    name: "",
-    email: "",
-    password: "",
-    msg: null,
-  };
-  componentDidMount() {
-    document.body.classList.toggle("register-page");
-    document.documentElement.addEventListener("mousemove", this.followCursor);
-  }
-  componentDidUpdate(prevProps) {
-    //console.log(prevProps);
-    const { error, isAuthenticated } = this.props;
-    const { email, password } = this.state;
-    //console.log(error);
-    if (error !== prevProps.error) {
-      if (error.id === "REGISTER_FAIL") {
-        if (!email || !password) {
-          this.setState({
-            msg: "Please enter all fields",
-          });
+    state = {
+      squares1to6: "",
+      squares7and8: "",
+      name: "",
+      email: "",
+      password: "",
+      msg: null,
+    };
+    componentDidMount() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.addEventListener("mousemove", this.followCursor);
+    }
+    componentDidUpdate(prevProps) {
+      //console.log(prevProps);
+      const { error, isAuthenticated } = this.props;
+      const { email, password } = this.state;
+      //console.log(error);
+      if (error !== prevProps.error) {
+        if (error.id === "REGISTER_FAIL") {
+          if (!email || !password) {
+            this.setState({
+              msg: "Please enter all fields",
+            });
+          } else {
+            this.setState({
+              msg: "Please enter your correct email and password.",
+            });
+          }
         } else {
+          /*if(!email || !password){
           this.setState({
-            msg: "Please enter your correct email and password.",
+            msg:"Please enter all fields"
+          })
+        }*/
+          this.setState({
+            msg: null,
           });
         }
-      } else {
-        /*if(!email || !password){
-        this.setState({
-          msg:"Please enter all fields"
-        })
-      }*/
-        this.setState({
-          msg: null,
-        });
+      }
+      if (isAuthenticated) {
+        this.toggleModal();
       }
     }
-    if (isAuthenticated) {
-      this.toggleModal();
-    }
-  }
-  toggleModal = () => {
-    this.props.clearErrors();
-    this.props.history.push("/");
-  };
-
-  componentWillUnmount() {
-    document.body.classList.toggle("register-page");
-    document.documentElement.removeEventListener(
-      "mousemove",
-      this.followCursor
-    );
-  }
-  followCursor = (event) => {
-    let posX = event.clientX - window.innerWidth / 2;
-    let posY = event.clientY - window.innerWidth / 6;
-    this.setState({
-      squares1to6:
-        "perspective(500px) rotateY(" +
-        posX * 0.05 +
-        "deg) rotateX(" +
-        posY * -0.05 +
-        "deg)",
-      squares7and8:
-        "perspective(500px) rotateY(" +
-        posX * 0.02 +
-        "deg) rotateX(" +
-        posY * -0.02 +
-        "deg)",
-    });
-  };
-  handleCreate = (email, password) => {
-    const user = {
-      email,
-      password,
+    toggleModal = () => {
+      this.props.clearErrors();
+      this.props.history.push("/");
     };
-    this.props.loginUser(user);
-  };
-  submitHandler = (e) => {
-    e.preventDefault();
-    const x = this.state.email;
-    const y = this.state.password;
-    this.handleCreate(x, y);
-  };
+  
+    componentWillUnmount() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.removeEventListener(
+        "mousemove",
+        this.followCursor
+      );
+    }
+    followCursor = (event) => {
+      let posX = event.clientX - window.innerWidth / 2;
+      let posY = event.clientY - window.innerWidth / 6;
+      this.setState({
+        squares1to6:
+          "perspective(500px) rotateY(" +
+          posX * 0.05 +
+          "deg) rotateX(" +
+          posY * -0.05 +
+          "deg)",
+        squares7and8:
+          "perspective(500px) rotateY(" +
+          posX * 0.02 +
+          "deg) rotateX(" +
+          posY * -0.02 +
+          "deg)",
+      });
+    };
+    handleCreate = (email, password) => {
+      const user = {
+        email,
+        password,
+      };
+      this.props.loginUser(user);
+    };
+    submitHandler = (e) => {
+      e.preventDefault();
+      const x = this.state.email;
+      const y = this.state.password;
+      this.handleCreate(x, y);
+    };
   render() {
     const { msg } = this.state;
     return (
       <>
+      {/* Temparary fix */}
+      <div style={{
+        height:"90px"
+      }}>
+      </div>
         <Navbar />
         <div className="wrapper">
           <div className="page-header">
             <div className="page-header-image" />
-            <div className="content">
-              <Container>
-                <Row>
-                  <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
-                    <div
-                      className="square square-7"
-                      id="square7"
-                      style={{ transform: this.state.squares7and8 }}
+            <section className="mid-section">
+            <div className="register">
+            <div className="row-1">
+              <div>
+              <div className="col d-flex justify-content-center"><h2 className="Registersty"><b>Sign in</b></h2></div>
+              </div>
+              <br></br>
+              
+              {/* <div><h5 className="msgbox">This is a message box</h5></div> */}
+                <Form 
+                  id='form' 
+                  className='flex flex-col'
+                  onSubmit={this.submitHandler} 
+                >
+                    <Input 
+                      type="text" 
+                      placeholder='Email' 
+                      className={classnames({
+                        "input-group-focus": this.state.emailFocus,
+                      })}
+                      onFocus={(e) =>
+                        this.setState({ emailFocus: true })
+                      }
+                      onBlur={(e) =>
+                        this.setState({ emailFocus: false })
+                      }
+                      onChange={(e) => {
+                        this.setState({ email: e.target.value });
+                        console.log(this.state.email);
+                      }}
                     />
-                    <div
-                      className="square square-8"
-                      id="square8"
-                      style={{ transform: this.state.squares7and8 }}
+                    <Input 
+                      type="password" 
+                      placeholder='Password'
+                      className={classnames({
+                        "input-group-focus": this.state.passwordFocus,
+                      })}
+                      onFocus={(e) =>
+                        this.setState({ passwordFocus: true })
+                      }
+                      onBlur={(e) =>
+                        this.setState({ passwordFocus: false })
+                      }
+                      onChange={(e) => {
+                        this.setState({ password: e.target.value });
+                      }} 
                     />
-                    <Card className="card-register">
-                      <CardHeader>
-                        <CardImg
-                          alt="..."
-                          src={require("assets/img/square-purple-1.png")}
-                        />
-                        <CardTitle tag="h4" className="ml-2">
-                          Sign In
-                        </CardTitle>
-                        <Row>
-                          <Button
-                            className="btn-icon btn-round"
-                            href="#pablo"
-                            target="_blank"
-                            color="neutral"
-                            onClick={(e) => e.preventDefault()}
-                            style={{ marginLeft: "20px" }}
-                          >
-                            <i className={"fab fa-twitter"} />
-                          </Button>
-                          <Button
-                            className="btn-icon btn-round"
-                            href="#pablo"
-                            target="_blank"
-                            color="neutral"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            <i className={"fab fa-facebook"} />
-                          </Button>
-                          <Button
-                            className="btn-icon btn-round"
-                            href="#pablo"
-                            target="_blank"
-                            color="neutral"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            <i className={"fab fa-google-plus-g"} />
-                          </Button>
-                        </Row>
-                      </CardHeader>
-                      <div>
-                        {msg ? (
-                          <h2
-                            style={{
-                              fontSize: "25px",
-                              color: "black",
-                              backgroundImage:
-                                "linear-gradient(to bottom right, pink, violet)",
-                              textAlign: "center",
-                              marginBottom: "30px",
-                            }}
-                          >
-                            {msg}
-                          </h2>
-                        ) : null}
-                      </div>
-                      <CardBody>
-                        <h6>Or Be Classic..</h6>
-                        <Form className="form">
-                          <InputGroup
-                            className={classnames({
-                              "input-group-focus": this.state.emailFocus,
-                            })}
-                          >
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="tim-icons icon-email-85" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Email"
-                              type="text"
-                              onFocus={(e) =>
-                                this.setState({ emailFocus: true })
-                              }
-                              onBlur={(e) =>
-                                this.setState({ emailFocus: false })
-                              }
-                              onChange={(e) => {
-                                this.setState({ email: e.target.value });
-                                console.log(this.state.email);
-                              }}
-                            />
-                          </InputGroup>
-                          <InputGroup
-                            className={classnames({
-                              "input-group-focus": this.state.passwordFocus,
-                            })}
-                          >
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="tim-icons icon-lock-circle" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Password"
-                              type="password"
-                              onFocus={(e) =>
-                                this.setState({ passwordFocus: true })
-                              }
-                              onBlur={(e) =>
-                                this.setState({ passwordFocus: false })
-                              }
-                              onChange={(e) => {
-                                this.setState({ password: e.target.value });
-                              }}
-                            />
-                          </InputGroup>
-                        </Form>
-                      </CardBody>
-                      <CardFooter>
+                          <Link to="/forgot-page">
+              <div className="forbut">
+                <div
+                  className="btnfg"
+                
+                >
+                  {" "}
+                  Forgot Password?
+                </div>
+                <br></br><br></br>
+                </div>
+                        </Link>
                         <Button
                           className="btn-round"
-                          color="primary"
+                          color="white"
                           size="lg"
                           onClick={this.submitHandler}
                         >
                           {" "}
                           Login
                         </Button>
-                        <Link to="/forgot-page">
-                          <Button
-                            className="btn-round"
-                            color="primary"
-                            size="lg"
-                          >
-                            Forgot Password?
-                          </Button>
-                        </Link>
+                </Form>
+                <CardFooter>
+                
+                        <div>
+                  <h4 style={{color:"black",textAlign:"center"}}>Dont have an account?</h4>
+                </div>
+                <div style={{textAlign:"center"}}>
+                <a href="/register-page" style={{color:"black",textAlign:"center",textDecoration: 'underline black'}}>Register</a>
+                </div>
+                        
                       </CardFooter>
-                    </Card>
-                  </Col>
-                </Row>
-                <div className="register-bg" />
-                <div
-                  className="square square-1"
-                  id="square1"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-2"
-                  id="square2"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-3"
-                  id="square3"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-4"
-                  id="square4"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-5"
-                  id="square5"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-6"
-                  id="square6"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-              </Container>
+            </div>
+            <div className="row-2">
+                <img src="https://github-production-user-asset-6210df.s3.amazonaws.com/99478938/275152281-153aa4ef-93ae-46b9-92e8-5639b16f463d.png" alt="" />
             </div>
           </div>
-          <Footer />
+        </section>
+          </div>
+          <Footer/>
         </div>
       </>
     );
   }
 }
-
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   isLoading: state.auth.isLoading,
   error: state.error,
 });
 
-export default compose(connect(mapStateToProps, { loginUser, clearErrors }))(
+export default compose(connect(mapStateToProps, { registerUser, clearErrors }))(
   SigninPage
 );
