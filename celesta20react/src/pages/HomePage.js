df436bde110349c1be808e7e590b2ff854d21f92
Genusuppal <harpranav2202@gmail.com>
2023-10-18 23:31:19 +0530

    Landing Page for Mobile and Hero Animation

--- celesta20react/src/pages/HomePage.js
+++ celesta20react/src/pages/HomePage.js
@@ -1,15 +1,28 @@
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbars/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./HomePage.module.css";
 import heroImg from "../assets/img/LandingPageHero.webp";
+import heroBg from "../assets/img/LandingPageHeroBgLayer.webp";
+import heroBgAlt from "../assets/img/LandingPageHeroBgLayerAlt.webp";
 
 export default function HomePage() {
+	const [heroBgLayer, setHeroBgLayer] = useState(window.innerWidth > 600 ? heroBg : heroBgAlt);
+
+	{
+		/* For Dev Purposes only */
+		function onWidthChange() {
+			window.innerWidth > 600 ? setHeroBgLayer(heroBg) : setHeroBgLayer(heroBgAlt);
+		}
+		window.addEventListener("resize", onWidthChange);
+	}
+
 	return (
 		<div>
 			<Navbar />
 			<div className={styles.landing_background_verdant}>
+				<img className={styles.landing_background_odyssey} src={heroBgLayer} />
 				<img className={styles.landing_hero} src={heroImg} />
 				<div className={styles.landing_title}>
 					<h1>Celesta ‘23</h1>
@@ -24,8 +37,8 @@ export default function HomePage() {
 						</Link>
 					</div>
 				</div>
-				<Footer />
 			</div>
+			<Footer marginTop={0} />
 		</div>
 	);
 }
