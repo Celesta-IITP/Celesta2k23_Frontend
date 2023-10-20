e9f582bc481ec4545550973d5b63e911e1fea14e
Genusuppal <harpranav2202@gmail.com>
2023-10-20 18:28:52 +0530

    Changed HomePage depending on login state

--- celesta20react/src/pages/HomePage.js
+++ celesta20react/src/pages/HomePage.js
@@ -1,44 +1,72 @@
+import React from "react";
+import { withRouter } from "react-router";
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbars/Navbar";
-import React, { useState } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { refreshPage } from "../redux/actions/authActions";
 import { Link } from "react-router-dom";
 import styles from "./HomePage.module.css";
 import heroImg from "../assets/img/LandingPageHero.webp";
 import heroBg from "../assets/img/LandingPageHeroBgLayer.webp";
 import heroBgAlt from "../assets/img/LandingPageHeroBgLayerAlt.webp";
 
-export default function HomePage() {
-	const [heroBgLayer, setHeroBgLayer] = useState(window.innerWidth > 600 ? heroBg : heroBgAlt);
-
-	{
-		/* For Dev Purposes only */
-		function onWidthChange() {
-			window.innerWidth > 600 ? setHeroBgLayer(heroBg) : setHeroBgLayer(heroBgAlt);
-		}
-		window.addEventListener("resize", onWidthChange);
+class HomePage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			userInfo: this.props.user ? this.props.user : {},
+			heroBgLayer: window.innerWidth > 600 ? heroBg : heroBgAlt,
+		};
 	}
 
-	return (
-		<div>
-			<Navbar />
-			<div className={styles.landing_background_verdant}>
-				<img alt="" className={styles.landing_background_odyssey} src={heroBgLayer} />
-				<img alt="Hero" className={styles.landing_hero} src={heroImg} />
-				<div className={styles.landing_title}>
-					<h1>Celesta ’23</h1>
-					<h2>The Techno-Management Fest of IIT Patna</h2>
+	onWidthChange = () => {
+		window.innerWidth > 600 ? this.setState({ heroBgLayer: heroBg }) : this.setState({ heroBgLayer: heroBgAlt });
+	}
+	componentDidMount() {
+		window.addEventListener("resize", this.onWidthChange);
+	}
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.onWidthChange);
+	}
 
-					<div>
-						<Link to="/register-page" className={styles.landing_register}>
-							REGISTER
-						</Link>
-						<Link to="/signin-page" className={styles.landing_login}>
-							LOG IN
-						</Link>
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<div className={styles.landing_background_verdant}>
+					<img alt="" className={styles.landing_background_odyssey} src={this.state.heroBgLayer} />
+					<img alt="Hero" className={styles.landing_hero} src={heroImg} />
+					<div className={styles.landing_title}>
+						<h1>Celesta ’23</h1>
+						<h2>The Techno-Management Fest of IIT Patna</h2>
+
+						<div>
+							{Object.keys(this.state.userInfo).length > 0 ? (
+								<Link to="/events-page" className={styles.landing_register}>
+									#getTechXited
+								</Link>
+							) : (
+								<>
+									<Link to="/register-page" className={styles.landing_register}>
+										REGISTER
+									</Link>
+									<Link to="/signin-page" className={styles.landing_login}>
+										LOG IN
+									</Link>
+								</>
+							)}
+						</div>
 					</div>
 				</div>
+				<Footer marginTop={0} />
 			</div>
-			<Footer marginTop={0} />
-		</div>
-	);
+		);
+	}
 }
+const mapStateToProps = (state) => ({
+	user: state.auth.user,
+	isLoading: state.auth.isLoading,
+	error: state.error,
+});
+export default withRouter(compose(connect(mapStateToProps, { refreshPage }))(HomePage));
